33242916df4f94bd0d26397b95e03eea7e426b57
Mik <info@slnt-opp.xyz>
2022-01-03 13:59:04 +0100

    func

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
+)
+
+//export sha256HashCertificate
+func sha256HashCertificate(in []byte) (string, error) {
+	block, _ := pem.Decode(in)
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return "", err
+	}
+	hash := sha256.Sum256(cert.Raw)
+	return hex.EncodeToString(hash[:]), nil
+}
